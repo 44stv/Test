ce15cff80964648fca7a4803a053a71d922697e6
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-02-22 19:06:17 +0200

    Created custom search tree

--- src/com/sturc/Abstraction/Task/Main.java
+++ src/com/sturc/Abstraction/Task/Main.java
@@ -4,20 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MyLinkedList list = new MyLinkedList(null);
+        SearchTree searchTree = new SearchTree(null);
 
         //String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
         String stringData = "5 7 1 4 9 8 6 2 3 0";
 
         String []data = stringData.split(" ");
         for (String s : data){
-            list.addItem(new Node(s));
+            searchTree.addItem(new Node(s));
         }
 
-        list.traverse(list.getRoot());
+        searchTree.traverse(searchTree.getRoot());
 
-        list.removeItem(new Node("5"));
+        searchTree.removeItem(new Node("4"));
 
-        list.traverse(list.getRoot());
+        searchTree.traverse(searchTree.getRoot());
     }
 }
