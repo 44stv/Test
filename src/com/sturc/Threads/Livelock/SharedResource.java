55f5239b47e0be66b9d9fd70252affe4a894d3a9
Stas <Satedart@gmail.com>
2018-03-13 22:52:06 +0200

    Livelock.

---
+++ src/com/sturc/Threads/Livelock/SharedResource.java
@@ -0,0 +1,18 @@
+package com.sturc.Threads.Livelock;
+
+public class SharedResource {
+
+    private Worker owner;
+
+    public SharedResource(Worker worker) {
+        this.owner = worker;
+    }
+
+    public Worker getOwner() {
+        return owner;
+    }
+
+    public synchronized void setOwner(Worker owner) {
+        this.owner = owner;
+    }
+}
