d0506fb41ddd6fb6c44b51ca4b942ed75751f62a
Stas <Satedart@gmail.com>
2018-03-19 19:43:21 +0200

    Implement setup() method.

--- src/com/sturc/UnitTesting/BankAccountTest.java
+++ src/com/sturc/UnitTesting/BankAccountTest.java
@@ -4,9 +4,16 @@ import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+    private BankAccount account;
+
+    @org.junit.Before
+    public void setup() {
+        account = new BankAccount("Stas", "Stas", 1000.0, BankAccount.CHECKING);
+        System.out.println("Running a test.");
+    }
+
     @org.junit.Test
     public void deposit() {
-        BankAccount account = new BankAccount("Stas", "Stas", 1000.0, BankAccount.CHECKING);
         double balance = account.deposit(200.0, true);
         assertEquals(1200.0, balance, 0);
     }
@@ -18,21 +25,18 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void getBalance_deposit() {
-        BankAccount account = new BankAccount("Stas", "Stas", 1000.0, BankAccount.CHECKING);
         account.deposit(200.0, true);
         assertEquals(1200.0, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void getBalance_withdraw() {
-        BankAccount account = new BankAccount("Stas", "Stas", 1000.0, BankAccount.CHECKING);
         account.withdraw(200.0, true);
         assertEquals(800.0, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void isChecking_true() {
-        BankAccount account = new BankAccount("Stas", "Stas", 1000.0, BankAccount.CHECKING);
         assertTrue("The account is not a checking account.", account.isChecking());
     }
 }
