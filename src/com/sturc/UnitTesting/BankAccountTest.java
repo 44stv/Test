19e23d0b73d9c9822932b561074813062e2b454e
Stas <Satedart@gmail.com>
2018-03-19 20:05:46 +0200

    Created BeforeClass and AfterClass methods.

--- src/com/sturc/UnitTesting/BankAccountTest.java
+++ src/com/sturc/UnitTesting/BankAccountTest.java
@@ -5,6 +5,12 @@ import static org.junit.Assert.*;
 public class BankAccountTest {
 
     private BankAccount account;
+    private static int count;
+
+    @org.junit.BeforeClass
+    public static void beforeClass() {
+        System.out.println("This executes before all test cases. Count = " + count++);
+    }
 
     @org.junit.Before
     public void setup() {
@@ -19,18 +25,24 @@ public class BankAccountTest {
     }
 
     @org.junit.Test
-    public void withdraw() {
-        fail("Yet to be implemented");
+    public void withdraw_branch() throws Exception {
+        double balance = account.withdraw(600.0, true);
+        assertEquals(400.0, balance, 0);
+    }
+
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void withdraw_notBranch() throws Exception {
+        account.withdraw(600.0, false);
     }
 
     @org.junit.Test
-    public void getBalance_deposit() {
+    public void getBalance_deposit() throws Exception {
         account.deposit(200.0, true);
         assertEquals(1200.0, account.getBalance(), 0);
     }
 
     @org.junit.Test
-    public void getBalance_withdraw() {
+    public void getBalance_withdraw() throws Exception {
         account.withdraw(200.0, true);
         assertEquals(800.0, account.getBalance(), 0);
     }
@@ -38,5 +50,15 @@ public class BankAccountTest {
     @org.junit.Test
     public void isChecking_true() {
         assertTrue("The account is not a checking account.", account.isChecking());
+    }
+
+    @org.junit.After
+    public void teardown() {
+        System.out.println("Count = " + count++);
+    }
+
+    @org.junit.AfterClass
+    public static void afterClass() {
+        System.out.println("This executes after all test cases. Count = " + count++);
     }
 }
