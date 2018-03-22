82f895a5387379d09dd720945818ddccc4f4c3c6
Stas <Satedart@gmail.com>
2018-03-22 23:33:01 +0200

    Change server to multi threading.

--- src/com/sturc/Networking/Server/Main.java
+++ src/com/sturc/Networking/Server/Main.java
@@ -1,9 +1,6 @@
 package com.sturc.Networking.Server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,18 +9,13 @@ public class Main {
     public static void main(String[] args) {
 
         try (ServerSocket serverSocket = new ServerSocket(5000)) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Client connected.");
-            BufferedReader input = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
 
             while (true) {
-                String echoString = input.readLine();
-                if (echoString.equals("exit")) {
-                    break;
-                }
-                output.println("Echo from server: " + echoString);
+/*                Socket socket = serverSocket.accept();
+                Echoer echoer = new Echoer(socket);
+                echoer.start();*/
+                new Echoer (serverSocket.accept()).start();
+                System.out.println("Client connected.");
             }
 
         } catch (IOException e) {
