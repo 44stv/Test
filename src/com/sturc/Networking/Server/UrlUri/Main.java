66561ca566a1e1a417b10430b258287dbcd35cc2
Stas <Satedart@gmail.com>
2018-03-25 21:32:01 +0300

    HttpURLConnection class and Apache httpComponents library.

--- src/com/sturc/Networking/Server/UrlUri/Main.java
+++ src/com/sturc/Networking/Server/UrlUri/Main.java
@@ -3,26 +3,43 @@ package com.sturc.Networking.Server.UrlUri;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.List;
-import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try {
-            URL url = new URL("http://example.org");
+            URL url = new URL("https://api.flickr.com/services/feeds/photos_public.gne?tags=cats");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Chrome");
+            connection.setReadTimeout(10000);
 
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.setDoOutput(true);
+
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
+
+            if (responseCode != 200) {
+                System.out.println("Error reading web page.");
+                System.out.println(connection.getResponseMessage());
+                return;
+            }
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String line;
+            while ((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            inputReader.close();
+
+
+/*            urlConnection.setDoOutput(true);
             urlConnection.connect();
-
-
-            BufferedReader input = new BufferedReader(
-                    new InputStreamReader(urlConnection.getInputStream()));
 
             Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
             for (Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
@@ -32,23 +49,10 @@ public class Main {
                 for (String string : value) {
                     System.out.println("Value: " + value);
                 }
-            }
+            }*/
 
-/*
-            String line = "";
-            while (line != null) {
-                line = input.readLine();
-                System.out.println(line);
-            }
-            input.close();
-*/
-
-
-//            URI uri = url.toURI();
-
-
-
-/*            System.out.println("Scheme = " + uri.getScheme());
+/*            URI uri = url.toURI();
+            System.out.println("Scheme = " + uri.getScheme());
             System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
             System.out.println("Authority = " + uri.getAuthority());
             System.out.println("User info = " + uri.getUserInfo());
