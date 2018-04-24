45d1f82c45cac357267960fdab05cbb60f1d30bd
Stas <Satedart@gmail.com>
2018-04-24 23:26:14 +0300

    FileChannel and ByteBuffer classes.

--- src/com/sturc/IO/NIO/Main.java
+++ src/com/sturc/IO/NIO/Main.java
@@ -1,7 +1,9 @@
 package com.sturc.IO.NIO;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -12,15 +14,38 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        try {
+
+        try (FileOutputStream binFile = new FileOutputStream("data.dat");
+             FileChannel binChannel = binFile.getChannel()) {
+
+            byte[] outputBytes = "Hello world!".getBytes();
+            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
+            int numBytes = binChannel.write(buffer);
+            System.out.println("Bytes written was: " + numBytes);
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(44);
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("Bytes written was: " + numBytes);
+
+            intBuffer.flip();
+            intBuffer.putInt(-44);
+            intBuffer.flip();
+            numBytes = binChannel.write(intBuffer);
+            System.out.println("Bytes written was: " + numBytes);
+
+
 //            FileInputStream file = new FileInputStream("data.txt");
 //            FileChannel channel = file.getChannel();
-            Path dataPath = FileSystems.getDefault().getPath("data.txt");
+
+/*            Path dataPath = FileSystems.getDefault().getPath("data.txt");
             Files.write(dataPath, "\nLine 5".getBytes("UTF-8"), StandardOpenOption.APPEND);
             List<String> lines = Files.readAllLines(dataPath);
             for (String line : lines) {
                 System.out.println(line);
-            }
+            }*/
+
         } catch (IOException e) {
             e.printStackTrace();
         }
