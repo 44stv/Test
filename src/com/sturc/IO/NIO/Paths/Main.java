f8a56b1a98e8e6d1383afa4b4a9422783569f937
Stas <Satedart@gmail.com>
2018-05-03 22:29:24 +0300

    NIO, createFile(), createDirectories() methods and files attributes, BasicFileAttributes interface.

--- src/com/sturc/IO/NIO/Paths/Main.java
+++ src/com/sturc/IO/NIO/Paths/Main.java
@@ -1,29 +1,38 @@
 package com.sturc.IO.NIO.Paths;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try {
-            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
-            Files.deleteIfExists(fileToDelete);
+/*            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+            Files.deleteIfExists(fileToDelete);*/
 
-/*            Path fileToMove = FileSystems.getDefault().getPath("Examples", "file1.txt");
-            Path destination = FileSystems.getDefault().getPath("Examples", "file2.txt");
-            Files.move(fileToMove, destination);*/
+//            Path fileToCreate = FileSystems.getDefault().getPath("Examples", "file2.txt");
+//            Files.createFile(fileToCreate);
 
-/*            Path sourceFile = FileSystems.getDefault().getPath("Examples", "file1.txt");
-            Path copyFile = FileSystems.getDefault().getPath("Examples", "file1copy.txt");
-            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir4");
+//            Files.createDirectory(dirToCreate);
 
-            sourceFile = FileSystems.getDefault().getPath("Examples", "Dir1");
-            copyFile = FileSystems.getDefault().getPath("Examples", "Dir4");
-            Files.copy(sourceFile, copyFile, StandardCopyOption.REPLACE_EXISTING);*/
+//            Path dirToCreate = FileSystems.getDefault().getPath("Examples", "Dir2\\Dir3\\Dir4\\Dir5\\Dir6");
+//            Files.createDirectories(dirToCreate);
+
+            Path filePath = FileSystems.getDefault().getPath("Examples", "Dir1\\file1.txt");
+            long byteSize = Files.size(filePath);
+            System.out.println("Size = " + byteSize);
+            System.out.println("Last modified = " + Files.getLastModifiedTime(filePath));
+
+            BasicFileAttributes fileAttributes = Files.readAttributes(filePath, BasicFileAttributes.class);
+            System.out.println("Size = " + fileAttributes.size());
+            System.out.println("Last modified = " + fileAttributes.lastModifiedTime());
+            System.out.println("Creation time = " + fileAttributes.creationTime());
+            System.out.println("Is directory = " + fileAttributes.isDirectory());
+            System.out.println("Is regular file = " + fileAttributes.isRegularFile());
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
