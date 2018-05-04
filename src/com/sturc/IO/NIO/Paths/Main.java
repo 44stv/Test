8ec0883d8acb56778c4491425021dc7470fe0db6
Stas <Satedart@gmail.com>
2018-05-04 22:36:54 +0300

    NIO, DirectoryStream and DirectoryStream.Filter.

--- src/com/sturc/IO/NIO/Paths/Main.java
+++ src/com/sturc/IO/NIO/Paths/Main.java
@@ -1,17 +1,17 @@
 package com.sturc.IO.NIO.Paths;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        try {
-/*            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
-            Files.deleteIfExists(fileToDelete);*/
+        /*        try {
+         *//*            Path fileToDelete = FileSystems.getDefault().getPath("Examples", "Dir1", "file1copy.txt");
+            Files.deleteIfExists(fileToDelete);*//*
 
 //            Path fileToCreate = FileSystems.getDefault().getPath("Examples", "file2.txt");
 //            Files.createFile(fileToCreate);
@@ -35,34 +35,25 @@ public class Main {
             System.out.println("Is regular file = " + fileAttributes.isRegularFile());
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        }*/
+
+//        DirectoryStream.Filter<Path> filter =
+//                new DirectoryStream.Filter<Path>() {
+//                    @Override
+//                    public boolean accept(Path path) throws IOException {
+//                        return (Files.isRegularFile(path));
+//                    }
+//                };
+        DirectoryStream.Filter<Path> filter = p -> Files.isRegularFile(p);
+
+        Path directory = FileSystems.getDefault().getPath("FileTree\\Dir2");
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
+            for (Path file : contents) {
+                System.out.println(file.getFileName());
+            }
+        } catch (IOException | DirectoryIteratorException e) {
+            e.printStackTrace();
         }
-
-
-
-
-
-/*        Path fileInDirectory = FileSystems.getDefault().getPath("WorkingDirectoryFile.txt");
-        printFile(fileInDirectory);
-//        Path fileInSubDirectory = FileSystems.getDefault().getPath("files", "SubdirectoryFile.txt");
-        Path fileInSubDirectory = Paths.get(".", "files", "SubdirectoryFile.txt");
-        printFile(fileInSubDirectory);
-//        Path outsideDirectory = Paths.get("D:\\GDrive\\Projects\\OutThere.txt");
-        Path outsideDirectory = Paths.get("D:\\", "GDrive\\Projects\\", "OutThere.txt");
-        printFile(outsideDirectory);
-
-        Path path1 = Paths.get(".");
-        System.out.println(path1.toAbsolutePath());
-
-        Path path2 = FileSystems.getDefault().getPath(".", "files", "..", "files", "SubdirectoryFile.txt");
-        System.out.println(path2.normalize().toAbsolutePath());
-
-        Path path4 = Paths.get("D:\\Test\\test", "test.txt");
-        System.out.println(path4.toAbsolutePath());
-
-        Path filePath = FileSystems.getDefault().getPath("files");
-        System.out.println("Exits = " + Files.exists(filePath));
-
-        System.out.println("Exits = " + Files.exists(path4));*/
     }
 
     private static void printFile(Path path) {
