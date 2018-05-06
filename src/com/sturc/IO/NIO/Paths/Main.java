5619612a7a9db90d5df2b2a0a35b30da9139794a
Stas <Satedart@gmail.com>
2018-05-06 23:03:37 +0300

    NIO, separator, temp files, FileStores.

--- src/com/sturc/IO/NIO/Paths/Main.java
+++ src/com/sturc/IO/NIO/Paths/Main.java
@@ -46,13 +46,31 @@ public class Main {
 //                };
         DirectoryStream.Filter<Path> filter = p -> Files.isRegularFile(p);
 
-        Path directory = FileSystems.getDefault().getPath("FileTree\\Dir2");
+//        Path directory = FileSystems.getDefault().getPath("FileTree\\Dir2");
+        Path directory = FileSystems.getDefault().getPath("FileTree" + File.separator + "Dir2");
         try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
             for (Path file : contents) {
                 System.out.println(file.getFileName());
             }
         } catch (IOException | DirectoryIteratorException e) {
             e.printStackTrace();
+        }
+
+        String separator = File.separator;
+        System.out.println(separator);
+        separator = FileSystems.getDefault().getSeparator();
+        System.out.println(separator);
+
+        try {
+            Path tempFile = Files.createTempFile("myapp", ".appext");
+            System.out.println("Temp file path = " + tempFile.toAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Iterable<FileStore> stores = FileSystems.getDefault().getFileStores();
+        for (FileStore store : stores) {
+            System.out.println(store + " " + store.type());
         }
     }
 
